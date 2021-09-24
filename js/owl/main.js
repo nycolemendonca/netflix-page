f75503fdca742ba52e19bd13b6c0767f3f3279d7
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2021-09-24 19:14:44 -0300

    upload

---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true, /* quando chegar ao último filme, o carrosel para */
+    margin:10, /* margem entre os filmes */
+    nav:false, /* navegador do carrossel (setas) */
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
