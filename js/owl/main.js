e731a28d7d89468c333fc5c8078b1a1e3a931046
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2021-09-25 11:13:33 -0300

    add carrossel

--- js/owl/main.js
+++ js/owl/main.js
@@ -1,8 +1,9 @@
 $('.owl-carousel').owlCarousel({
     loop:true, /* quando chegar ao último filme, o carrosel para */
     margin:10, /* margem entre os filmes */
-    nav:false, /* navegador do carrossel (setas) */
-    responsive:{
+    nav: false, /* navegador do carrossel (setas) */
+    responsive:
+    {
         0:{
             items:1
         },
