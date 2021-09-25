bd3e4c96e758a5a15ccb6aabac5b6cf3b1300670
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2021-09-25 12:46:51 -0300

    atualizacao

--- js/owl/main.js
+++ js/owl/main.js
@@ -1,17 +1,17 @@
 $('.owl-carousel').owlCarousel({
-    loop:true, /* quando chegar ao último filme, o carrosel para */
+    loop:true, /* quando chegar ao último filme, o carrosel reinicia */
     margin:10, /* margem entre os filmes */
     nav: false, /* navegador do carrossel (setas) */
     responsive:
     {
-        0:{
-            items:1
+        0:{ /* pixels */
+            items:1 /* números de boxes de filme */
         },
-        600:{
-            items:3
+        600:{ /* pixels */
+            items:3 /* números de boxes de filme */
         },
-        1000:{
-            items:5
+        1000:{ /* pixels */
+            items:5 /* números de boxes de filme */
         }
     }
 })
